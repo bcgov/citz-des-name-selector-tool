8ce61ed1f778c5041bc0e474fa3e195efc45701a
bburciaga <burciagab@protonmail.com>
2025-12-10 12:30:53 -0800

    adding project

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import './app.css';
+  import svelteLogo from './assets/svelte.svg';
+  import viteLogo from '/vite.svg';
+  import Counter from './lib/Counter.svelte';
+ import { Input } from "$lib/components/ui/input/index.js";
+ import { Label } from "$lib/components/ui/label/index.js";
+</script>
+
+<main class="flex items-center justify-center min-h-screen bg-gray-100">
+ 
+  <form>
+    <div class="min-h-100 flex flex-col justify-evenly border-b border-t border-gray-900/10 p-12">
+
+      <div class="flex w-full max-w-sm flex-col gap-1.5">
+        <Label for="first-name">First name</Label>
+        <Input type="text" id="first-name" placeholder="" />
+        <p class="text-muted-foreground text-sm">Enter your first name.</p>
+      </div>
+
+      <div class="flex w-full max-w-sm flex-col gap-1.5">
+        <Label for="last-name">Last name</Label>
+        <Input type="text" id="last-name" placeholder="" />
+        <p class="text-muted-foreground text-sm">Enter your last name.</p>
+      </div>
+
+    </div>
+  </form>
+
+</main>
+
+<style>
+</style>
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import Button from "$lib/components/ui/button/button.svelte";
+  let count: number = $state(0)
+  const increment = () => {
+    count += 1
+  }
+</script>
+
+<Button onclick={increment}>
+  count is {count}
+</Button>
