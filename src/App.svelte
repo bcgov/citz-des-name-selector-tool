98378887140372b0b6babc6d9976bdb4f0000d05
bburciaga <bb_host@pm.me>
2026-03-09 15:25:35 -0700

    changed to header and BC Design token header

--- src/App.svelte
+++ src/App.svelte
@@ -8,9 +8,18 @@
 </script>
 
 <main class="flex flex-col items-center justify-center min-h-screen">
-    <Navbar />
+    <!--<Navbar />-->
+    <header class="bcds-header">
+        <div class="bcds-header--container">
+            <a>Logo</a>
+            <div class="bcds-header--line"></div>
+            <div class="bcds-header--title">
+                Step
+            </div>
+        </div>
+    </header>
 
-    <form class="mt-15 absolute top-0 px-7 pb-15">
+    <form class="top-0 px-7 pb-15">
         <!-- <form class="bg-blue-50 mt-15 absolute top-0 px-7 pb-15"> -->
 
         <FullName />
@@ -33,4 +42,39 @@
 </main>
 
 <style>
+    .bcds-header {
+        box-sizing: border-box;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-around;
+        background-color: var(--surface-color-forms-default);
+        border-bottom-color: var(--surface-color-border-default);
+        border-bottom-style: solid;
+        border-bottom-width: var(--layout-border-width-small);
+        min-height: 65px;
+        padding: var(--layout-padding-none) var(--layout-padding-medium);
+        width: 100%;
+    }
+
+    .bcds-header--container {
+        flex-grow: 1;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        gap: var(--layout-padding-medium);
+        max-width: 1100px;
+    }
+
+    .bcds-header--line {
+        background-color: var(--surface-color-border-default);
+        width: 1px;
+        height: 32px;
+    }
+
+    .bcds-header--title {
+        flex-grow: 1;
+        font: var(--typography-bold-h4);
+        color: var(--typography-color-primary);
+    }
 </style>
