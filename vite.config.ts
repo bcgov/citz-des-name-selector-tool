855512deac3b392c79c21e8b814895aa23d10fb6
Tyler Krys <Tyler.Krys@gov.bc.ca>
2026-03-17 11:40:59 -0700

    Add Vitest configuration to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,13 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import path from 'path';
 
+// Vitest config
+const vitestResolve = process.env.VITEST
+    ? {
+          conditions: ['browser'],
+      }
+    : undefined;
+
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [tailwindcss(), svelte()],
@@ -10,5 +17,6 @@ export default defineConfig({
         alias: {
             $lib: path.resolve('./src/lib'),
         },
+        ...vitestResolve,
     },
 });
