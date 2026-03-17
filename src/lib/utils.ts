f62985f7b5733c32856480f792c95bda4314784e
Tyler Krys <Tyler.Krys@gov.bc.ca>
2026-03-17 11:36:44 -0700

    Remove unused Typography.js dependency

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,5 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
-import Typography from 'typography';
 import '@bcgov/bc-sans/css/BC_Sans.css';
 
 export function cn(...inputs: ClassValue[]) {
