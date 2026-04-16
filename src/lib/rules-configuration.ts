a9275d8e069bf62d40867164fc29310af9f54669
Tyler Krys <Tyler.Krys@gov.bc.ca>
2026-04-16 10:31:12 -0700

    Update rules configuration to add legal name rules

--- src/lib/rules-configuration.ts
+++ src/lib/rules-configuration.ts
@@ -1,9 +1,8 @@
 export const rules = {
-    // TODO: Are there rules for "legal name" or are these rules all for "alternate name"?
-    alternateName: {
+    legalName: {
         first: {
             length: {
-                maximumPerName: 12,
+                maximumPerName: 50,
                 errorText:
                     'Your Legal First Name exceeds the maximum allowable length for your first name',
             },
@@ -16,12 +15,12 @@ export const rules = {
         middle: {
             length: {
                 0: {
-                    maximum: 12,
+                    maximum: 50,
                     errorText:
                         'Your first Legal Middle Name exceeds the maximum length of a middle name',
                 },
                 1: {
-                    maximum: 12,
+                    maximum: 50,
                     errorText:
                         'Your second middle name exceeds the maximum length of a middle name',
                 },
@@ -34,7 +33,7 @@ export const rules = {
         },
         last: {
             length: {
-                maximum: 18,
+                maximum: 50,
                 errorText:
                     'Your Legal Last Name exceeds the maximum allowable length for your last name',
             },
@@ -42,6 +41,51 @@ export const rules = {
                 maximum: 1,
                 errorText:
                     'The amount of Legal Last Names exceeds the maximum number of names allowed',
+            },
+        },
+    },
+    alternateName: {
+        first: {
+            length: {
+                maximumPerName: 12,
+                errorText:
+                    'Your Alternate First Name exceeds the maximum allowable length for your first name',
+            },
+            count: {
+                maximum: 1,
+                errorText:
+                    'The amount of Alternate First Names exceeds the maximum number of names allowed',
+            },
+        },
+        middle: {
+            length: {
+                0: {
+                    maximum: 12,
+                    errorText:
+                        'Your first Alternate Middle Name exceeds the maximum length of a middle name',
+                },
+                1: {
+                    maximum: 12,
+                    errorText:
+                        'Your second middle name exceeds the maximum length of a middle name',
+                },
+            },
+            count: {
+                maximum: 2,
+                errorText:
+                    'The amount of Alternate Middle Names exceeds the maximum number of names allowed',
+            },
+        },
+        last: {
+            length: {
+                maximum: 18,
+                errorText:
+                    'Your Alternate Last Name exceeds the maximum allowable length for your last name',
+            },
+            count: {
+                maximum: 1,
+                errorText:
+                    'The amount of Alternate Last Names exceeds the maximum number of names allowed',
             },
         },
     },
