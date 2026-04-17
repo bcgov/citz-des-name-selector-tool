76779a1cac26820c8f677137c7094fef3ef7ca8e
Tyler Krys <Tyler.Krys@gov.bc.ca>
2026-04-17 16:27:12 -0700

    Add Welcome, Background, and Consideration pages to App

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,9 @@
     import './app.css';
     import Header from './lib/components/ui/header/header.svelte';
     import Footer from './lib/components/ui/footer/Footer.svelte';
+    import Welcome from '$lib/Welcome.svelte';
+    import Background from '$lib/Background.svelte';
+    import Considerations from '$lib/Considerations.svelte';
     import FullName from './lib/FullName.svelte';
     import AltName from './lib/AltName.svelte';
     import '@bcgov/design-tokens/css/variables.css';
@@ -13,11 +16,23 @@
     <Header />
 
     <form class="grow top-0 px-7 pb-15">
+        {#if step.value === 0}
+            <Welcome />
+        {/if}
+
         {#if step.value === 1}
-            <FullName />
+            <Background />
         {/if}
 
         {#if step.value === 2}
+            <Considerations />
+        {/if}
+
+        {#if step.value === 3}
+            <FullName />
+        {/if}
+
+        {#if step.value === 4}
             <AltName />
         {/if}
 
