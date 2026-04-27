8e39386ffff4b26c96d5462655acb5da5d9094d9
Tyler Krys <Tyler.Krys@gov.bc.ca>
2026-04-27 13:49:03 -0700

    Bump step state max value to 5 for ViewName page

--- src/lib/shared.svelte.ts
+++ src/lib/shared.svelte.ts
@@ -28,7 +28,7 @@ export const altName = $state({
     },
 });
 
-export const step = $state({ value: 0, max: 4 });
+export const step = $state({ value: 0, max: 5 });
 
 // export const errors = $state({ value: [] });
 class Errors {
