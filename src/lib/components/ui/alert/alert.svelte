dba0066662eceacc09d8cd40dce881628d815c6d
bburciaga <bb_host@pm.me>
2026-04-29 13:39:26 -0700

    added icons to warning and success alerts

--- src/lib/components/ui/alert/alert.svelte
+++ src/lib/components/ui/alert/alert.svelte
@@ -1,9 +1,19 @@
 <script lang="ts">
+    import CircleCheck from '../../../../../public/CircleCheck.svg';
+    import TriangleExclamation from '../../../../../public/TriangleExclamation.svg';
     let {message, success = false} = $props();
 
-    let clasName = success ? "bcds-TextField--success" : "bcds-TextField--warning";
+    let className = success ? "bcds-TextField--success" : "bcds-TextField--warning";
 </script>
 
-<p class={clasName}>
-    {message}
-</p>
+<div class={`bcds-TextField--alert ${className}`}>
+    {#if success}
+        <img src={CircleCheck} alt="circle-check-icon" />
+    {:else}
+        <img src={TriangleExclamation} alt="warning-icon" />
+    {/if}
+    <p>{message}</p>
+</div>
+
+<style>
+</style>
